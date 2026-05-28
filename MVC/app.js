810ac3484d0a63403fa3244e068c93bf7b3d085a
עומר ברדה <omerbarda5@gmail.com>
2026-05-28 23:28:48 +0300

    MVC example

---
+++ MVC/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+var app = express()
+
+const articles = require('./routes/articles');
+app.use('/articles', articles);
+
+app.set('view engine', 'ejs');
+
+app.listen(8080)
